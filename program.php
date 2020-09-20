5c96b27e5501a20b637262a592fe29441f78edaa
Kyle Eggleston <kyle@dreamsense.com>
2020-09-19 22:25:53 -0600

    Added an assignment

--- program.php
+++ program.php
@@ -6,22 +6,29 @@
 Hello World
 </pre>
 
+<?php $id = 0;?>
+
 <table style="width:100%;font-size:14pt;border:1px solid #000">
   <tr>
     <th colspan="3">Basics of Java Programming</td>
   </tr>
   <tr>
-    <td>0</td>
+    <td><?php echo $id; $id++;?></td>
     <td>Basics</td>
     <td><a href="/programming/hello.php">Hello World</a></td>
   </tr>
   <tr>
-    <td>1</td>
+    <td><?php echo $id; $id++;?></td>
     <td>Basics</td>
     <td><a href="/programming/variables.php">Variables</a></td>
   </tr>
   <tr>
-    <td>2</td>
+    <td><?php echo $id; $id++;?></td>
+    <td>Basics</td>
+    <td><a href="/programming/variables2.php">More Variables</a></td>
+  </tr>  
+  <tr>
+    <td><?php echo $id; $id++;?></td>
     <td>Basics</td>
     <td><a href="/programming/comments.php">Comments</a></td>
   </tr>
---
+++ programming/variables2.php
@@ -0,0 +1,17 @@
+<?php $title="More Variables";?>
+<?php include('../header.php');?>
+<?php $footer="../footer/program.php";?>
+
+<p>Here's an example program you can output using variables. Actually use
+  variables, don't just output the data using strings. Make your output
+  look like the following, try your best to line it all up.</p>
+
+<pre class="bash">
++--------------------------------------------------------+
+|                                             Fred Jones |
+|                                                        |
+|                                              President |
++--------------------------------------------------------+
+</pre>
+
+<?php include('../footer.php');?>
