90b9e714bc27ea5579f7d09dec9da43d45f46bcd
Kyle Eggleston <kyle@dreamsense.com>
2020-09-21 18:45:47 -0600

    Corrected program

--- programming/doWhile.php
+++ programming/doWhile.php
@@ -6,12 +6,16 @@
   is met.</p>
 
 <pre class="code">
-int i = 0;
+public class DoWhile {
+  public static void main(String[] args) {
+    int i = 0;
 
-do {
-  System.out.println(i);
-  i++;
-} while (i <= 10);
+    do {
+      System.out.println(i);
+      i++;
+    } while (i <= 10);
+  }
+}
 </pre>
 
 <p>The output from the above is as follows:</p>
