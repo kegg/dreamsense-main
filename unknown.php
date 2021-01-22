b6d21899f7afede7d770c99d45b7a44b23a757b7
Kyle Eggleston <kyle@dreamsense.com>
2021-01-21 18:42:38 -0700

    Added Turth In The Fold

--- unknown.php
+++ unknown.php
@@ -2,4 +2,7 @@
 <?php $footer="footer/stories.php";?>
 <?php include("header.php");?>
 
+<p><a href="pdf/fold.pdf" target="_blank">Truth In The Fold</a> - Aliens and Humans just don't
+  get along.</p>
+
 <?php include("footer.php");?>
